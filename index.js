a12ec26b8205a9c3f91e4fcd50f7183bd344e540
Josh Young <jyoung@vecna.com>
2015-06-10 17:19:15 -0400

    Initial work to allow more configuration of quick-sip.

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 module.exports = function(gulp) {
-  var browserifyBundler, copyResources, copyResource, buildStyles, buildApp, configureBrowserify,
+  var browserifyBundler,
+    failOnError = false,
     transformStack = [],
     nonResources = 'js|css|scss',
     fs = require('fs'),
     gulpLoadPlugins = require('gulp-load-plugins'),
     $ = gulpLoadPlugins({}),
+    del = require('del'),
     log = require('color-log'),
     pathLib = require('path'),
     source = require('vinyl-source-stream'),
@@ -17,40 +19,50 @@ module.exports = function(gulp) {
       scripts: 'scripts',
       styles: 'styles',
       templates: 'scripts/**/templates',
-      dist: 'dist'
-    };
+      dist: 'dist',
+      rootJs: 'main',
+      rootScss: 'app'
+    },
+    buildTasks = [
+      'copy-resources',
+      'build-styles',
+      'build-app'
+    ];
+
+  function defaultPath(path, defaultPath) {
+    if (path && path[0] === '.' || path[0] === '/') {
+      return path;
+    }
+    return defaultPath + path;
+  };
 
   /* Current date-time printer */
-  var currentDateTime = function() {
-    var date = new Date();
-    var datetime = '';
+  function currentDateTime() {
+    var date = new Date(),
+        datetime = '';
     datetime += date.getHours() + ':' + date.getMinutes() + ':' + date.getSeconds() + ' ';
     datetime += (date.getMonth() + 1) + '/' + date.getDate() + '/' + date.getFullYear();
     return datetime;
   };
 
-  /* Relative path */
-  var relativePath = function(path) {
-    return path.substring(process.cwd().length + 1);
-  };
-
   /* Browserify bundler */
   watchify.args.debug = ($.util.env.type !== 'production');
-  configureBrowserify = function() {
-    browserifyBundler.add('./' + paths.app + '/' + paths.scripts + '/main');
-    for (var i = 0; i < transformStack.length; i++) {
-      browserifyBundler.transform(transformStack[i]);
+  function configureBrowserify(browserifyBundler) {
+    var transformStackIndex;
+    browserifyBundler.add(defaultPath(paths.rootJs, './' + paths.app + '/' + paths.scripts + '/'));
+    for (transformStackIndex = 0; transformStackIndex < transformStack.length; transformStackIndex++) {
+      browserifyBundler.transform(transformStack[transformStackIndex]);
     }
   };
 
   /* Clean the paths.dist directory */
-  gulp.task('clean', function() {
-    return gulp.src(paths.dist, {read: false})
-      .pipe($.rimraf());
+  gulp.task('clean', function(callback) {
+    log.mark('[CLEAN] deleting ' + paths.dist);
+    del(paths.dist, callback);
   });
 
   /* Copy all resources to dist */
-  copyResources = function() {
+  function copyResources() {
     var bytes = 0;
     var startTime = +new Date();
     return gulp.src([
@@ -71,7 +83,7 @@ module.exports = function(gulp) {
   gulp.task('copy-resources', copyResources);
 
   /* Handle single resource events for dist */
-  copyResource = function(evt) {
+  function copyResource(evt, callback) {
     var status = evt.type;
     var path = evt.path;
     var relPath = pathLib.relative('./app', evt.path);
@@ -95,15 +107,15 @@ module.exports = function(gulp) {
         .pipe(gulp.dest(destPath));
     } else if (status === 'deleted') {
       log.mark('[DELETED] --> ' + relPath);
-      return gulp.src(destPath + '/' +relPath)
-        .pipe($.rimraf());
+      del(destPath + '/' + relPath, callback);
     }
   };
 
   /* Build all styles */
-  buildStyles = function() {
+  function buildStyles() {
     return gulp.src([paths.app + '/**/*.scss'])
     .pipe($.sass({
+      file: defaultPath(paths.rootScss, paths.app),
       includePaths: [],
       onSuccess: function(err) {
         log.mark('[SASS] ' + err.css.length + ' bytes written (' + (err.stats.duration / 1000.0) + ' seconds)');
@@ -119,12 +131,15 @@ module.exports = function(gulp) {
   gulp.task('build-styles', buildStyles);
 
   /* Reduce all javascript to app.js */
-  buildApp = function() {
+  function buildApp() {
     return browserifyBundler.bundle()
-      .on('error', function(err, b) {
+      .on('error', function(err) {
         delete err.stream;
         log.error('[BROWSERIFY] @ ' + currentDateTime());
         log.warn(err.toString());
+        if (!browserifyBundler.continueOnError && failOnError) {
+          throw err;
+        }
         return true;
       })
       .pipe(source('app.js'))
@@ -139,13 +154,9 @@ module.exports = function(gulp) {
   /* Full build */
   gulp.task('build', function(callback) {
     browserifyBundler = browserify(watchify.args);
-    configureBrowserify();
-    runSequence('clean',
-      [
-        'copy-resources',
-        'build-styles',
-        'build-app'
-      ], function() {
+    configureBrowserify(browserifyBundler);
+    runSequence('clean', buildTasks,
+      function() {
         log.mark('[BROWSERIFY] complete!');
         callback();
       });
@@ -154,7 +165,8 @@ module.exports = function(gulp) {
   /* Watch build */
   gulp.task('watch', function() {
     browserifyBundler = watchify(browserify(watchify.args));
-    configureBrowserify();
+    browserifyBundler.continueOnError = true;
+    configureBrowserify(browserifyBundler);
     gulp.watch(paths.app + '/' + paths.styles + '/**/*.scss', buildStyles);
     gulp.watch([
       paths.app + '/**/*.*',
@@ -164,12 +176,7 @@ module.exports = function(gulp) {
     browserifyBundler.on('log', function(data) {
       log.mark('[BROWSERIFY] ' + data.toString());
     });
-    runSequence('clean',
-      [
-        'copy-resources',
-        'build-styles',
-        'build-app'
-      ]);
+    runSequence('clean', buildTasks);
   });
 
   /* Returns public configuration API */
@@ -180,6 +187,36 @@ module.exports = function(gulp) {
 
     nonResources: function(nrsc) {
       nonResources = nrsc;
+    },
+
+    setRootJS: function(rootJs) {
+      paths.rootJs = rootJs;
+    },
+
+    setRootSCSS: function(rootScss) {
+      paths.rootScss = rootScss;
+    },
+
+    setDist: function(dist) {
+      paths.dist = dist;
+    },
+
+    setNoScss: function() {
+      var buildStylesIndex = buildTasks.indexOf('build-styles');
+      if (buildStylesIndex > -1) {
+        buildTasks.splice(buildStylesIndex, 1);
+      }
+    },
+
+    setNoCopyResources: function() {
+      var copyResourcesIndex = buildTasks.indexOf('copy-resources');
+      if (copyResourcesIndex > -1) {
+        buildTasks.splice(copyResourcesIndex, 1);
+      }
+    },
+
+    setFailOnError: function() {
+      failOnError = true;
     }
   };
 };
